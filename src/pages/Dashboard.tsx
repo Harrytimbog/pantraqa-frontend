e7e9a0e17bcf52b87724270cf780ca6ea6b19421
Timilehin Arigbede <harriemannie@gmail.com>
2025-04-17 01:01:19 +0100

    Complete App Authentication

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,26 @@
+import { useAuth } from "../context/AuthContext";
 
 const Dashboard = () => {
-    return (
-        <div>Dashboard</div>
-    )
-}
+    const { user } = useAuth();
 
-export default Dashboard
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center bg-background text-secondary px-4">
+            <h1 className="text-3xl font-bold mb-4">Welcome to Pantraqa Dashboard</h1>
+
+            {user ? (
+                <div className="bg-white p-6 rounded shadow-md w-full max-w-md text-center">
+                    <p className="text-lg">
+                        <span className="font-semibold text-primary">Email:</span> {user.email}
+                    </p>
+                    <p className="text-lg mt-2">
+                        <span className="font-semibold text-accent">Role:</span> {user.role}
+                    </p>
+                </div>
+            ) : (
+                <p className="text-red-500">No user is logged in.</p>
+            )}
+        </div>
+    );
+};
+
+export default Dashboard;
