60caf491b8a2caeec4f94a85fc3b42354f800ab5
Timilehin Arigbede <harriemannie@gmail.com>
2025-04-20 20:49:58 +0100

    Add StockIn and StockOut to main layout

--- src/pages/stocks/StockIn.tsx
+++ src/pages/stocks/StockIn.tsx
@@ -1,11 +1,9 @@
-// src/pages/stocks/StockIn.tsx
 
 import StockForm from "../../components/UI/StockForm";
 
 const StockIn = () => {
     return (
         <div className="py-10 px-4">
-            <h1 className="text-2xl font-bold text-secondary mb-6 text-center">Stock In</h1>
             <StockForm action="in" />
         </div>
     );
